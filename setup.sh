5f9b3345dddddd99fdb22835408b6ea9b82e842f
MonsieurC <anthony.gab@outlook.fr>
2024-11-24 16:59:51 +0100

    Testing GitHub Action

--- setup.sh
+++ setup.sh
@@ -2,7 +2,11 @@
 
 echo "Install setup for Monsieur Canard !"
 echo "You need to have an internet connection and sudo rights."
-echo "Press Enter to continue..."
+
+/**
+ * ! Check if the user is root, unable for github actions
+ */
+# echo "Press Enter to continue..."
 # read -n 1 -s
 
 # if [ $? -eq 0]; then
@@ -12,11 +16,19 @@ echo "Press Enter to continue..."
 # 	exit 1
 # fi
 
+/**
+ * ! Update and upgrade packages
+ */
 echo "Update and upgrade packages..."
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y build-essential
-sudo apt-get install -y git curl wget python3 python3-pip g++ make gcc
+sudo apt-get install -y \
+\
+	git curl wget python3 python3-pip g++ make gcc \
 
+/**
+ * ! Install Vscode
+ */
 echo "Installing VSCode..."
 sudo apt install -y software-properties-common apt-transport-https
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
@@ -24,9 +36,33 @@ sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/v
 sudo apt update
 sudo apt install -y code
 
+
+/**
+ * ! Install Mozilla Firefox
+ */
 echo "Installing Mozilla Firefox..."
 sudo apt install -y firefox
 
+/**
+ * ! Install Docker
+ */
+echo "Installing Docker..."
+sudo apt install -y docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker $USER
+
+
+/**
+ * ! Install Docker Compose
+ */
+echo "Installing Docker Compose..."
+sudo apt install -y docker-compose
+
+
+/**
+ * ! Install Oh My Zsh
+ */
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	echo "Install Oh My Zsh..."
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -35,6 +71,43 @@ else
 	echo "Oh My Zsh already installed."
 fi
 
+/**
+ * ! Install Zsh plugins
+ */
+echo "Install Zsh plugins..."
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+	git clone https://github.com/zsh-users/zsh-autosuggestions.git "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions"
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-completions" ]; then
+	git clone https://github.com/zsh-users/zsh-completions.git "$HOME/.oh-my-zsh/custom/plugins/zsh-completions"
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-history-substring-search" ]; then
+	git clone https://github.com/zsh-users/zsh-history-substring-search.git "$HOME/.oh-my-zsh/custom/plugins/zsh-history-substring-search"
+fi
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-navigation-tools" ]; then
+	git clone https://github.com/zsh-users/zsh-navigation-tools.git "$HOME/.oh-my-zsh/custom/plugins/zsh-navigation-tools"
+fi
+
+/**
+ * ! Install z command
+ */
+
+if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/z" ]; then
+	echo "Install z command..."
+	git clone https://github.com/rupa/z.git "$HOME/.oh-my-zsh/custom/plugins/z"
+fi
+
+
+/**
+ * ! Install Powerlevel10k
+ */
 if [ ! -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
 	echo "Installation de Powerlevel10k..."
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
