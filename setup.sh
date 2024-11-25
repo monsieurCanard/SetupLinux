7c1e92750351af26abf8b465f627682ab56b896b
MonsieurC <anthony.gab@outlook.fr>
2024-11-25 15:44:32 +0100

    Go testing in VM

--- setup.sh
+++ setup.sh
@@ -3,28 +3,57 @@
 echo "Install setup for Monsieur Canard !"
 echo "You need to have an internet connection and sudo rights."
 
-/**
- * ! Check if the user is root, unable for github actions
- */
-# echo "Press Enter to continue..."
-# read -n 1 -s
+# Detect the Linux distribution
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$ID
+else
+    echo "Unsupported distribution"
+    exit 1
+fi
 
-# if [ $? -eq 0]; then
-# 	echo "Let's go !"
-# else
-# 	echo "Exiting..."
-# 	exit 1
-# fi
+# Function to install packages for Debian/Ubuntu
+install_debian() {
+	echo "Debian/Ubuntu detected ! Installing packages..."
+    sudo apt-get update -y && sudo apt-get upgrade -y
+    sudo apt-get install -y build-essential git curl valgrind wget python3 python3-pip g++ make gcc clang-15
+    sudo apt-get install -y software-properties-common apt-transport-https
+    wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+    sudo apt update
+    sudo apt install -y code firefox docker.io docker-compose
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo usermod -aG docker $USER
+}
 
-#  * ! Update and upgrade packages
+# Function to install packages for Fedora
+install_fedora() {
+	echo "Fedora detected ! Installing packages..."
+    sudo dnf update -y
+    sudo dnf install -y @development-tools git curl valgrind wget python3 python3-pip gcc-c++ make clang
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+    sudo dnf check-update
+    sudo dnf install -y code firefox docker docker-compose
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo usermod -aG docker $USER
+}
 
-echo "Update and upgrade packages..."
-sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install -y build-essential
-sudo apt-get install -y \
-\
-	git curl wget python3 python3-pip g++ make gcc clang-15 \
-
+# Install packages based on the detected distribution
+case $OS in
+    ubuntu|debian)
+        install_debian
+        ;;
+    fedora)
+        install_fedora
+        ;;
+    *)
+        echo "Unsupported distribution: $OS ! Sorry :("
+        exit 1
+        ;;
+esac
 
 #  * ! Install Vscode
 
@@ -59,12 +88,18 @@ sudo apt install -y docker-compose
 #  * ! Install Oh My Zsh
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	echo "Install Oh My Zsh..."
-	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	chsh -s $(which zsh)
 else 
 	echo "Oh My Zsh already installed."
 fi
 
+echo alias work='cd ~/Documents/Work' >> "$HOME/.zshrc"
+
+echo alias cl='clear' >> "$HOME/.zshrc"
+echo alias vleak='valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes' >> "$HOME/.zshrc"
+echo alias vfd='valgrind --track-fds=yes' >> "$HOME/.zshrc"
+echo alias vchild='valgrind --track-children=yes' >> "$HOME/.zshrc"
 
 #  * ! Install Zsh plugins
 
@@ -93,6 +128,9 @@ else
 	echo "Powerlevel10k est déjà installé."
 fi
 
-if [ ! grep -q "source $HOME/.p10k.zsh" "$HOME/.zshrc" ]; then
+if ! grep -q "source $HOME/.p10k.zsh" "$HOME/.zshrc"; then
 	echo "source $HOME/.p10k.zsh" >> "$HOME/.zshrc"
 fi
+
+# Clean up unnecessary packages
+sudo apt-get autoremove -y
