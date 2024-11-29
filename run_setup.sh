147b95ca7f01c92d6b1a2fdcfd8f9518165b6fc7
MonsieurC <anthony.gab@outlook.fr>
2024-11-29 09:17:17 +0100

    add docker

--- run_setup.sh
+++ run_setup.sh
@@ -3,6 +3,7 @@
 chmod +x setup_files/setup_all_language.sh
 chmod +x setup_files/setup_terminal.sh
 chmod +x setup_files/setup_essential.sh
+chmod +x setup_files/setup_docker.sh
 
 ./setup_files/setup_essential.sh
 wait $!
